1d8ba508fa03e58d978e5ea98b61cf9a52189a85
Veronika0136 <Artemova331@gmail.com>
2023-01-07 21:28:26 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+function liItem (array){
+  const list =[];
+
+  array.map(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  list.push(li);
+  }) 
+  
+  const ul = document.querySelector('#ingredients');
+  ul.append(...list);
+}
+
+liItem(ingredients);
+
+
+
+
+
+
