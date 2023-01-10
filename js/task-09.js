363148f7291d56fed1651a10f0eb57ef4da6f481
Veronika0136 <Artemova331@gmail.com>
2023-01-10 19:51:58 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+btn.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor(){
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+
+}
+
