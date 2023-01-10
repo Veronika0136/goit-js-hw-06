2b0a7729cdbe39f718631e175b828012001f1ec3
Veronika0136 <Artemova331@gmail.com>
2023-01-10 19:27:43 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onSubmit);
 
+function onSubmit(evt){
+    evt.preventDefault();
+    const formElements = evt.currentTarget.elements;
+    if (formElements.password.value === ''|| formElements.email.value === ''){
+        alert (`Не все поля заполнены!`)
+    } else {
+    const formData = new FormData(evt.currentTarget);
+       formData.forEach((element, value) => {
+       console.log(value, element)
+       const btn = evt.currentTarget.elements[2];
+       btn.setAttribute('type', 'reset');
+       });
+    }
+}
