c3136404734a33737aad295ab0ef6bb7873a1f32
Veronika0136 <Artemova331@gmail.com>
2023-01-11 19:13:36 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,14 +4,26 @@ form.addEventListener('submit', onSubmit);
 function onSubmit(evt){
     evt.preventDefault();
     const formElements = evt.currentTarget.elements;
-    if (formElements.password.value === ''|| formElements.email.value === ''){
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (password === ''|| email === ''){
         alert (`Не все поля заполнены!`)
     } else {
-    const formData = new FormData(evt.currentTarget);
-       formData.forEach((element, value) => {
-       console.log(value, element)
-       const btn = evt.currentTarget.elements[2];
-       btn.setAttribute('type', 'reset');
-       });
+        const formResult ={email,password,}
+        console.log (formResult);
+        const btn = evt.currentTarget.elements[2];
+        btn.setAttribute('type', 'reset');
+
+
+
+
+
+    // const formData = new FormData(evt.currentTarget);
+    //    formData.forEach((element, value) => {
+    //    console.log({value, element})
+    //    const btn = evt.currentTarget.elements[2];
+    //    btn.setAttribute('type', 'reset');
+    //    });
     }
 }
